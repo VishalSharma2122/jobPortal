76adf57a7f32e443b3fc23fe0a5818ea73a870d9
Vishal sharma <vishalsharma3456g@gmail.com>
2024-12-18 00:33:56 +0530

    first commit in frontend

---
+++ frontend/src/shared/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='bg-white'>
+<div>
+    <h1 className='text-2xl font-bold'>Job<span className='text-[#f83002]'>Portal</span></h1>
+</div>
+
+    </div>
+  )
+}
+
+export default Navbar
