0931cf4060412deadae4924dc8697c3fefa5372c
Your Name <you@example.com>
2025-03-20 18:12:57 +0530

    Add job-related components and update Redux slices for job management

--- frontend/src/shared/Navbar.jsx
+++ frontend/src/shared/Navbar.jsx
@@ -7,7 +7,6 @@ import {
 import { Avatar, AvatarImage } from "../components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 function Navbar() {
   const {user} = useSelector(store=>store.auth);
