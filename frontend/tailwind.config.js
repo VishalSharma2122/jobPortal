2677a44f9009506249a7d955059cfd229652d57d
vishalsharma2122 <vishalsharma3456g@gmail.com>
2025-03-30 22:39:46 +0530

    fixed issue

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
 	darkMode: ["class"],
-	content: [
-	  './pages/**/*.{js,jsx}',
-	  './components/**/*.{js,jsx}',
-	  './app/**/*.{js,jsx}',
-	  './src/**/*.{js,jsx}',
-	],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	prefix: "",
 	theme: {
 	  container: {
@@ -74,4 +69,5 @@ module.exports = {
 	  },
 	},
 	plugins: [require("tailwindcss-animate")],
-  }
+  };
+  
